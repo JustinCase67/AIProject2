1c9c5e0d1df376ef6dc1dfb736fefe3a5a569f79
Arzyel <emeric_rivard@hotmail.com>
2024-05-15 20:26:19 -0400

    finalized the two external methods to get all points or end points only

--- dev/physics_sim.py
+++ dev/physics_sim.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 
-class phys_sim:
+class Phys_sim:
 
     @staticmethod
     def vector_angle(x, y):
@@ -22,11 +22,11 @@ class phys_sim:
         angle_rad = math.radians(angle)
         vix = vi * math.cos(angle_rad)
         viy = vi * math.sin(angle_rad)
-        return phys_sim.position(pix, vix, 0, temps), phys_sim.position(piy, viy, g, temps)
+        return Phys_sim.position(pix, vix, 0, temps), Phys_sim.position(piy, viy, g, temps)
 
     @staticmethod
     def coordo_final_2d_speed_separated(vix, viy, temps,g , pix, piy):
-        return phys_sim.position(pix, vix, 0, temps), phys_sim.position(piy, viy, g, temps)
+        return Phys_sim.position(pix, vix, 0, temps), Phys_sim.position(piy, viy, g, temps)
 
     @staticmethod
     def final_speed_1d(vi, a, t):
@@ -37,7 +37,7 @@ class phys_sim:
         angle_rad = math.radians(angle)
         vix = vi * math.cos(angle_rad)
         viy = vi * math.sin(angle_rad)
-        return phys_sim.final_speed_1d(vix, 0, t), phys_sim.final_speed_1d(viy, a, t)
+        return Phys_sim.final_speed_1d(vix, 0, t), Phys_sim.final_speed_1d(viy, a, t)
 
     @staticmethod
     def explo_impulsion_finale(coordo, vi, explo_force):
@@ -59,7 +59,7 @@ class phys_sim:
     def v_explo_new_missiles(number_missiles, explo_angle, vi):
         vix, viy = vi
         c = (vix ** 2 + viy ** 2) ** 0.5
-        angle_origin_vector = phys_sim.vector_angle(vix, viy)
+        angle_origin_vector = Phys_sim.vector_angle(vix, viy)
         explo_angle_rad = math.radians(explo_angle)
 
         mvix = c * math.cos(explo_angle_rad + angle_origin_vector)
@@ -69,7 +69,6 @@ class phys_sim:
         m2vix = c * math.cos(angle_origin_vector - explo_angle_rad)
         m2viy = c * math.sin(angle_origin_vector - explo_angle_rad)
 
-        print("angle of speed vector : ", phys_sim.vector_angle(vix, viy))
 
         return (vix, viy), (mvix, mviy), (m2vix, m2viy)
 
@@ -82,7 +81,7 @@ class phys_sim:
         arc_coordinates[0] = coordo_ini
         arc_coordinates[-1] = coordo_final
         for i in range(1, nb_iteration - 1):
-            arc_coordinates[i] = phys_sim.coordo_final_2d_speed_separated(speed_ini_x, speed_ini_y, i*iteration_time,g, coordo_ini[0], coordo_ini[1])
+            arc_coordinates[i] = Phys_sim.coordo_final_2d_speed_separated(speed_ini_x, speed_ini_y, i * iteration_time, g, coordo_ini[0], coordo_ini[1])
 
         return arc_coordinates
 
@@ -90,7 +89,7 @@ class phys_sim:
     def get_new_missiles_points(nb_missiles, coordo_init, tuples_coordo_final, tuples_speeds, g, final_time, iteration_time):
         missiles_points_array = []
         for i in range(nb_missiles):
-            points = phys_sim.get_arc_points(coordo_init,tuples_coordo_final[i],tuples_speeds[i][0],tuples_speeds[i][1],g, final_time[i], iteration_time)
+            points = Phys_sim.get_arc_points(coordo_init, tuples_coordo_final[i], tuples_speeds[i][0], tuples_speeds[i][1], g, final_time[i], iteration_time)
             missiles_points_array.append(points)
         return tuple(missiles_points_array)
 
@@ -100,64 +99,85 @@ class phys_sim:
         all_coordo = []
         time = []
         for object in all_object_speeds:
-            time_at_y0 = phys_sim.time_at_yf(object[1], g, coordo_ini[1], final_pos_y)
+            time_at_y0 = Phys_sim.time_at_yf(object[1], g, coordo_ini[1], final_pos_y)
             time.append(time_at_y0)
-            pos_x = (phys_sim.position(coordo_ini[0], object[0], 0, time_at_y0))
+            pos_x = (Phys_sim.position(coordo_ini[0], object[0], 0, time_at_y0))
             pos = (pos_x,final_pos_y )
             all_coordo.append(pos)
 
         return tuple(time), tuple(all_coordo)
 
     @staticmethod
-    def get_all_points(coordo_init, coordo_cluster, initial_speed_x, initial_speed_y, g, time_to_cluster, missiles_final_time,
-                       missiles_speed_ini, missiles_final_coordo,iteration_time,nb_missiles ):
+    def get_all_points(coordo_init, coordo_cluster, initial_speed_x, initial_speed_y, g, time_to_cluster, missiles_final_time, missiles_speed_ini, missiles_final_coordo,iteration_time,nb_missiles ):
 
-        return phys_sim.get_arc_points(coordo_init,coordo_cluster,initial_speed_x, initial_speed_y,g,time_to_cluster,iteration_time),\
-               phys_sim.get_new_missiles_points(nb_missiles, coordo_cluster, missiles_final_coordo, missiles_speed_ini,g,missiles_final_time,iteration_time)
+        inital_arc_coordinates = Phys_sim.get_arc_points(coordo_init, coordo_cluster, initial_speed_x, initial_speed_y, g, time_to_cluster, iteration_time)
+        objects_arc_coordinates = Phys_sim.get_new_missiles_points(nb_missiles, coordo_cluster, missiles_final_coordo, missiles_speed_ini, g, missiles_final_time, iteration_time)
 
-
-
-vi = 10
-g = 9.81
-t = 2.3
-coordo_init = (0,50)
-
-angle = 0
-exploF = 1.5
-exploAngle = 10
-print("time at 0", phys_sim.time_at_yf(0, g, coordo_init[1], 0))
-final_speed = phys_sim.final_speed_2d(vi, g, t, angle)
-print("final speed : ", final_speed)
-coordo = phys_sim.coordo_final_2d(vi, angle, t, g, coordo_init[0], coordo_init[1])
-print("coordo :", coordo)
-explo_final = phys_sim.explo_impulsion_finale(coordo, final_speed, exploF)
-print("explo final vitesse: ", explo_final)
-new_missiles = phys_sim.v_explo_new_missiles(0, exploAngle, explo_final)
-print("new missiles vitesse: ", new_missiles)
+        return inital_arc_coordinates, objects_arc_coordinates
 
 
+    @staticmethod
+    def get_final_coordinates_from_start_data(initial_speed, time_to_split, coordo_init, initial_angle, g,  split_force, split_angle, nb_object_split, final_pos_y):
 
+        coordo_at_split = Phys_sim.coordo_final_2d(initial_speed, initial_angle, time_to_split, g, coordo_init[0], coordo_init[1])
+        speed_at_split = Phys_sim.final_speed_2d(initial_speed, g, time_to_split, initial_angle)
+        speed_ref_after_split = Phys_sim.explo_impulsion_finale(coordo_at_split, speed_at_split, split_force)
+        speed_all_objects_after_split = Phys_sim.v_explo_new_missiles(nb_object_split, split_angle, speed_ref_after_split)
 
-arc_coordinates_1 = phys_sim.get_arc_points(coordo_init,coordo,10,0, g, t, 1)
-print("first arc : ", arc_coordinates_1)
+        return Phys_sim.get_final_coordinates_missile(speed_all_objects_after_split, g, coordo_at_split, final_pos_y)
 
-final_time_coordo_x = phys_sim.get_final_coordinates_missile(new_missiles, g, coordo, 0)
-print("missiles arc final", final_time_coordo_x[1])
+    @staticmethod
+    def get_all_points_from_start_data(initial_speed, time_to_split, coordo_init, initial_angle, g,  split_force, split_angle, nb_object_split, final_pos_y, iteration_time):
 
-all_missiles_points = phys_sim.get_new_missiles_points(3, coordo, final_time_coordo_x[1], new_missiles,g,final_time_coordo_x[0],1)
-print(all_missiles_points)
+        coordo_at_split = Phys_sim.coordo_final_2d(initial_speed, initial_angle, time_to_split, g, coordo_init[0], coordo_init[1])
+        speed_at_split = Phys_sim.final_speed_2d(initial_speed, g, time_to_split, initial_angle)
+        speed_ref_after_split = Phys_sim.explo_impulsion_finale(coordo_at_split, speed_at_split, split_force)
+        speed_all_objects_after_split = Phys_sim.v_explo_new_missiles(nb_object_split, split_angle, speed_ref_after_split)
+
+        final_objects_time_coordinates = Phys_sim.get_final_coordinates_missile(speed_all_objects_after_split, g, coordo_at_split, final_pos_y)
+
+        angle_rad = math.radians(initial_angle)
+        speed_ini_x = initial_speed * math.cos(angle_rad)
+        speed_ini_y = initial_speed * math.sin(angle_rad)
+
+        return Phys_sim.get_all_points(coordo_init, coordo_at_split, speed_ini_x, speed_ini_y, g, time_to_split, final_objects_time_coordinates[0],speed_all_objects_after_split, final_objects_time_coordinates[1], iteration_time, nb_object_split)
 
 
 
 
-all_points = phys_sim.get_all_points(coordo_init,coordo,10,0,g,t, final_time_coordo_x[0],new_missiles,final_time_coordo_x[1],1,3)
-print(all_points)
+
+def main():
+    vi = 10
+    g = 9.81
+    time_to_split = 2.3
+    coordo_init = (0,50)
+    final_pos_y = 0
+    initial_angle = 0
+    split_force = 1.5
+    split_angle = 10
+    nb_splitting_objects = 3
+    iteration_time_s = 1
+
+    final_time_coordo_func = Phys_sim.get_final_coordinates_from_start_data(vi,time_to_split,coordo_init,initial_angle,g,split_force,split_angle,nb_splitting_objects,final_pos_y)
+    print("final_time_coordo_func: ", final_time_coordo_func)
+
+    all_points_func = Phys_sim.get_all_points_from_start_data(vi,time_to_split,coordo_init, initial_angle, g, split_force, split_angle, nb_splitting_objects,final_pos_y,iteration_time_s )
+    print("all_points", all_points_func)
+    for i in range(5000):
+        final_time_coordo_func = Phys_sim.get_final_coordinates_from_start_data(vi, time_to_split, coordo_init,
+                                                                                initial_angle, g, split_force, split_angle,
+                                                                                nb_splitting_objects, final_pos_y)
+
+        all_points_func = Phys_sim.get_all_points_from_start_data(vi, time_to_split, coordo_init, initial_angle, g,
+                                                                  split_force, split_angle, nb_splitting_objects,
+                                                                  final_pos_y, iteration_time_s)
+
+    print("done")
 
 
-#print(arc_coordinates)
-#arc_coordinates[0] = (1,56)
-#print(arc_coordinates)
 
+if __name__ == '__main__':
+    main()
 
 
 
